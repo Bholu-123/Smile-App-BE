a3a8aa49b3919952394a3d4a6765c94415c968f6
Bholu-tiwari <bholutiwari121@gmail.com>
2023-05-13 15:57:10 +0530

    fixed for email nodemailer

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -9,10 +9,10 @@ const sendEmailController = async (req, res) => {
     // Send the email using the sendEmail function from the emailUtility file
     await sendEmail(to, subject, text);
 
-    res.status(200).json({ message: 'Email sent successfully' });
+    res.status(200).json({ message: "Email sent successfully" });
   } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).json({ error: 'Failed to send email' });
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "Failed to send email" });
   }
 };
 
